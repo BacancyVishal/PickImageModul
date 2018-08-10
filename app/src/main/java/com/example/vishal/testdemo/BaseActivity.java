d41979ede3562f404d57d5f84ebbf057abd41677
BacancyVishal <vishal.j.patel@bacancytechnology.com>
2018-08-10 15:30:22 +0530

    Initial commit

---
+++ app/src/main/java/com/example/vishal/testdemo/BaseActivity.java
@@ -0,0 +1,179 @@
+package com.example.vishal.testdemo;
+
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.ProgressDialog;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
+import android.media.RingtoneManager;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.provider.Settings;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.TextView;
+
+
+import com.example.vishal.testdemo.permission.MultiplePermissionCallback;
+import com.example.vishal.testdemo.permission.Permission;
+import com.example.vishal.testdemo.permission.PermissionActivity;
+import com.example.vishal.testdemo.permission.PermissionUtils;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+/**
+ * Created by sumeet on 5/10/17.
+ */
+
+public class BaseActivity extends AppCompatActivity {
+
+    private static String displayName = "";
+    public ImageSelectUtils imageSelectUtils;
+    public static PermissionActivity permissionActivity;
+    public static int TYPE_WIFI = 1;
+    public static int TYPE_MOBILE = 2;
+    public static int TYPE_NOT_CONNECTED = 0;
+    private boolean internetConnected = true;
+    private boolean isMultiplePermissionRequested = false;
+    private MultiplePermissionCallback multiplePermissionCallback;
+    private List<String> permissionsToRequest = new ArrayList<>();
+    static String channelId = "channel-01";
+    static String channelName = "Channel Name";
+    static int importance = NotificationManager.IMPORTANCE_HIGH;
+
+    private static final int PERMISSION_REQUEST_CODE = 10 << 1;
+    private ProgressDialog mProgressDialog;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        imageSelectUtils = new ImageSelectUtils(BaseActivity.this);
+        permissionActivity = new PermissionActivity(BaseActivity.this);
+
+    }
+
+
+    public void requestPermissions(Permission[] permissions, MultiplePermissionCallback multiplePermissionCallback) {
+        if (PermissionUtils.isMarshmallowOrHigher()) {
+            isMultiplePermissionRequested = true;
+            this.multiplePermissionCallback = multiplePermissionCallback;
+
+            for (Permission permission : permissions) {
+                if (!PermissionUtils.isGranted(this, permission)) {
+                    permissionsToRequest.add(permission.toString());
+                }
+            }
+
+            if (!permissionsToRequest.isEmpty()) {
+                requestPermissions(permissionsToRequest.toArray(new String[permissionsToRequest.size()]),
+                        PERMISSION_REQUEST_CODE);
+            }
+        }
+    }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        permissionActivity.onPermissionsResult(requestCode, permissions, grantResults);
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+    BroadcastReceiver broadcastReceiver = new BroadcastReceiver() {
+        private boolean firstConnect = true;
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            // internet lost alert dialog method call from here...
+
+
+            final ConnectivityManager connectivityManager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+            final NetworkInfo activeNetInfo = connectivityManager.getActiveNetworkInfo();
+            String status = getConnectivityStatusString(context);
+            if (activeNetInfo == null) {
+                if (firstConnect) {
+                    // do subroutines here
+                    //AlertUtils.showSimpleAlert(BaseActivity.this, "Internet stop working");
+                    firstConnect = false;
+                }
+            } else {
+                //AlertUtils.showSimpleAlert(BaseActivity.this, "Internet start working");
+                firstConnect = true;
+            }
+        }
+    };
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        registerInternetCheckReceiver();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        unregisterReceiver(broadcastReceiver);
+    }
+
+    /**
+     * Method to register runtime broadcast receiver to show snackbar alert for internet connection..
+     */
+    private void registerInternetCheckReceiver() {
+        IntentFilter internetFilter = new IntentFilter();
+        internetFilter.addAction("android.net.wifi.STATE_CHANGE");
+        internetFilter.addAction("android.net.conn.CONNECTIVITY_CHANGE");
+        registerReceiver(broadcastReceiver, internetFilter);
+    }
+
+    public static String getConnectivityStatusString(Context context) {
+        int conn = getConnectivityStatus(context);
+        String status = null;
+        if (conn == TYPE_WIFI) {
+            status = "Wifi enabled";
+        } else if (conn == TYPE_MOBILE) {
+            status = "Mobile data enabled";
+        } else if (conn == TYPE_NOT_CONNECTED) {
+            status = "Not connected to Internet";
+        }
+        return status;
+    }
+
+    public static int getConnectivityStatus(Context context) {
+        ConnectivityManager cm = (ConnectivityManager) context
+                .getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
+        if (null != activeNetwork) {
+            if (activeNetwork.getType() == ConnectivityManager.TYPE_WIFI)
+                return TYPE_WIFI;
+
+            if (activeNetwork.getType() == ConnectivityManager.TYPE_MOBILE)
+                return TYPE_MOBILE;
+        }
+        return TYPE_NOT_CONNECTED;
+    }
+
+    public void showLog(String msg) {
+
+        if (BuildConfig.DEBUG) Log.e("showLog", msg);
+    }
+
+
+}
